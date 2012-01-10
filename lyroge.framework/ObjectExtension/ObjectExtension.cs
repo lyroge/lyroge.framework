2323ce084acec0d6407a74d02223ae03f3698a21
lyroge <lyroge@foxmail.com>
2012-01-10 18:18:08 +0800

    对object，bool对象进行了扩展

---
+++ lyroge.framework/ObjectExtension/ObjectExtension.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lyroge.framework.ObjectExtension
+{
+    public static class ObjectExtension
+    {
+        //调用示例
+        //object o = null;
+        //o.IfNull(() => Response.Write("here"));
+        public static void IsNull(this object obj, Action action)
+        {
+            if (obj == null)
+                action();
+        }
+
+        #region 对布尔类型的扩展，简化if else操作
+        public static void IsTrue(this bool b, Action action)
+        {
+            if (b == true)
+                action();
+        }
+
+        public static void IsFalse(this bool b, Action action)
+        {
+            if (b == false)
+                action();
+        }
+
+        public static void IsTrueOrFalse(this bool b, Action action0, Action action1)
+        {
+            if (b == true)
+                action0();
+            else
+                action1();
+        }
+        #endregion
+
+        //public static TResult IfNull<TResult>(this object obj, Func<TResult> func)
+        //{
+        //    if (obj == null)
+        //        return func();
+        //}
+    }
+}
