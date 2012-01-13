48b9a6d8b5258cbaec20995a6a7005e6fe359409
lyroge <lyroge@foxmail.com>
2012-01-13 15:29:10 +0800

    添加 RequestHelper 帮助类

--- lyroge.framework/ObjectExtension/ObjectExtension.cs
+++ lyroge.framework/ObjectExtension/ObjectExtension.cs
@@ -5,16 +5,35 @@ using System.Text;
 
 namespace lyroge.framework.ObjectExtension
 {
-    public static class ObjectExtension
+    public static partial class ObjectExtension
     {
-        //调用示例
-        //object o = null;
-        //o.IfNull(() => Response.Write("here"));
-        public static void IsNull(this object obj, Action action)
+        #region Object对象扩展
+
+        /// <summary>
+        /// 如果对象为空，那么执行一个动作
+        /// 调用示例
+        /// object o = null;
+        /// o.IfNull(() => Response.Write("here"));
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="action"></param>
+        public static void IfNull(this object obj, Action action)
         {
             if (obj == null)
                 action();
         }
+
+        /// <summary>
+        /// 对象如果是不空则返回值有func决定
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static T IfNotNull<T>(this object obj, Func<object, T> func)
+        {
+            return obj == null ? default(T) : func(obj);
+        }
+        #endregion
 
         #region 对布尔类型的扩展，简化if else操作
         public static void IsTrue(this bool b, Action action)
@@ -35,6 +54,29 @@ namespace lyroge.framework.ObjectExtension
                 action0();
             else
                 action1();
+        }
+        #endregion
+
+        #region 字符串类型对象扩展
+        /// <summary>
+        /// 如果字符串为空字符串或者null 那么返回数值0
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string EmptyOrNull2Zero(this string str)
+        {
+            return str.EmptyOrNull2String("0");
+        }
+
+        /// <summary>
+        /// 如果字符串是空字符或者null， 那么返回val所指的值
+        /// </summary>
+        /// <param name="str"></param>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static string EmptyOrNull2String(this string str, string val)
+        {
+            return string.IsNullOrEmpty(str) ? val : str;
         }
         #endregion
     }
