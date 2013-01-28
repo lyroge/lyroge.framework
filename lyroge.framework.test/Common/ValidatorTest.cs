3fded8bd4fcb7a2873e7b55089e6145961968b8a
lyroge <lyroge@foxmail.com>
2013-01-28 15:45:49 +0800

    添加web测试项目，对Common.Validator类写单元测试

---
+++ lyroge.framework.test/Common/ValidatorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lyroge.framework.Common;
+
+namespace lyroge.framework.Test.Common
+{
+    [TestClass]
+    public class ValidatorTest
+    {
+        [TestMethod]
+        public void ValidateEmail()
+        {
+            Assert.IsTrue(Validator.IsEmail("lyroge@foxmail.com"));
+            Assert.IsFalse(Validator.IsEmail("lyroge#foxmail.c"));
+            Assert.IsFalse(Validator.IsEmail("lyroge@foxmailc"));
+        }
+    }
+}
